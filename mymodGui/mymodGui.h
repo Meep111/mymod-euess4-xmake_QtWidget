b64319b7b70f9f2b823ffa4a3f508504004e77b4
Meep111 <ltess748@gmail.com>
2024-12-29 23:48:31 +0100

    Update mymodGui.h
    dokumention

--- mymodGui/mymodGui.h
+++ mymodGui/mymodGui.h
@@ -1,16 +1,37 @@
 #pragma once
 
+// Includes the QtWidgets library for creating graphical user interfaces.
 #include <QtWidgets/QWidget>
+
+// Includes the generated UI header file for the `mymodGui` class.
 #include "ui_mymodGui.h"
 
+// The `mymodGui` class represents the graphical user interface (GUI) for the application.
+// It is a QWidget-based class that provides the main interface functionality.
 class mymodGui : public QWidget
 {
-    Q_OBJECT
+    Q_OBJECT // Macro enabling Qt's signal and slot mechanism.
 
 public:
+    /**
+     * @brief Constructs the `mymodGui` class.
+     * 
+     * @param parent The parent widget. Defaults to `nullptr` for a top-level widget.
+     */
     mymodGui(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the `mymodGui` class.
+     * 
+     * Cleans up resources and performs necessary teardown for the GUI.
+     */
     ~mymodGui();
 
 private:
+    /**
+     * @brief The UI object for managing and interacting with the GUI components.
+     * 
+     * This object is auto-generated from the associated Qt Designer `.ui` file.
+     */
     Ui::mymodGuiClass ui;
 };
